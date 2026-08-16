05012b17fc963fc12cbbcb5057fd677a5a57fb79
gintaras 65 <gjuskev@gmail.com>
2026-08-16 20:53:52 +0300

    Parašytos media užklausos. Baigtas projektas

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Optional parameters
 
   // loop: true,
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
-		type: 'fraction',
+    el: ".swiper-pagination",
+    type: "fraction",
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button-next',
-    prevEl: '.slider-button-prev',
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
   },
+});
 
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
 
+menuBtn.addEventListener('click', ()=> {
+  menuList.classList.toggle('menu__open');
 });
